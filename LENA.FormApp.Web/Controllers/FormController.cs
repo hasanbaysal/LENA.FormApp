dd3f172889e67ebc435c04df05f9607ded5908a8
Hasan Baysal <33396565+hasanbaysal@users.noreply.github.com>
2023-04-27 06:23:19 +0300

    form add

---
+++ LENA.FormApp.Web/Controllers/FormController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LENA.FormApp.Web.Controllers
+{
+    public class FormController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet()]
+        public IActionResult Forms(int id)
+        {
+
+            return View();
+        }
+    }
+}
